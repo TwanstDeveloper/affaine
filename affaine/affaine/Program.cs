6479f146950a6b7584d52a9b9017ff18953ceb92
Twanst Codes <98418359+TwanstDeveloper@users.noreply.github.com>
2023-02-28 02:29:02 +0300

    file code

---
+++ affaine/affaine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace affaine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "tyuiohgygy";
+            Program ob = new Program();
+            Console.WriteLine(ob.enc(x));
+        }
+        string enc(string msg)
+        {
+            string cipher = "";
+            int a = 5;
+            int b = 10;
+            for (int i = 0; i < msg.Length; i++)
+            {
+                if (char.IsUpper(msg[i]))
+                {
+                    cipher=cipher+(char)(((((a*(msg[i]-65))+b))%26)+25);
+                }else if (char.IsLower(msg[i]))
+                {
+                    cipher = cipher + (char)(((((a * (msg[i] - 97)) + b)) % 26) + 97);
+                }
+            }
+            return cipher;
+        }
+    }
+}
